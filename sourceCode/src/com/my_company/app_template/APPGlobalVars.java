1b17b3c70b70f4a4e8ad5f45be541c1e31fa52f2
bryguy0804 <bryguy0804@gmail.com>
2012-10-01 21:48:33 +0000

    Updating to the latest application code

---
+++ sourceCode/src/com/my_company/app_template/APPGlobalVars.java
@@ -0,0 +1,9 @@
+package com.my_company.app_template;
+
+public class APPGlobalVars {
+  public static String SCR_PAUSE_CTL;
+
+  protected APPGlobalVars() {
+    SCR_PAUSE_CTL = "";
+  }// end constructor
+}//end APPGlobalVars()
